bbfa54fafb8b67abbfea36a38aa83da6f32d6f6c
Bruno Solano <brunobsolano@gmail.com>
2021-05-21 11:47:44 -0400

    subindo exercicio

---
+++ Soletrando-bandeiraCartao/script.js
@@ -0,0 +1,11 @@
+
+function soletrar() {
+    let palavra = document.getElementById("palavra").value
+    palavra.length
+    for (let cont=0; cont<palavra.length; cont++) {
+        alert(palavra[cont])
+    }
+ }
+
+
+ 
