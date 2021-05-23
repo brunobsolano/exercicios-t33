1cd94bef66369b99ff73ce3b2d9dd5a519f32df5
Bruno Solano <brunobsolano@gmail.com>
2021-05-23 19:47:05 -0400

    atualziando lista de convidados

--- lista-convidados-t33/script.js
+++ lista-convidados-t33/script.js
@@ -1,40 +1,82 @@
-let convidados = [
+let convidados = []
+
+let perguntas = [
     {
-        nome: "Jão da Silva",
-        email: "jao@gmail.com",
-        idade: 20
-    },
-    {
-        nome: "Maria da Silva",
-        email: "mah@gmail.com",
-        idade: 23
+        titulo: "Você tem fluxo de caixa?",
+        respostas: [
+            {
+                titulo: "Não tenho",
+                diagnostico: "Aí complica ein..."    
+            },
+            {
+                titulo: "Tenho em planilha",
+                diagnostico: "Mais ou menos..."    
+            },
+            {
+                titulo: "Tenho em sistema",
+                diagnostico: "Tá jóia!"    
+            }
+        ]
     }
 ]
 
+let posicaoEdicao
+
 function carregarLista() {
-    let divLista = document.getElementById("lista")
-    divLista.innerHTML = ""
-    for(let cont = 0; cont < convidados.length; cont++) {
-        /**
-         *  <tr>
-                <td>Jão da Silva</td>
-                <td>jao@gmail.com</td>
-                <td>20</td>
-            </tr>
-         */
-        divLista.innerHTML += "<tr><td>" + convidados[cont].nome + "</td><td>" + convidados[cont].email + "</td><td>" + convidados[cont].idade + "</td></tr>"
+    let tbody = document.getElementById("lista")
+    tbody.innerHTML = "" // "limpa"
+    for (let cont = 0; cont < convidados.length; cont++) {
+        tbody.innerHTML += `
+        <tr>
+            <td>${convidados[cont].nome}</td>
+            <td>${convidados[cont].email}</td>
+            <td>${convidados[cont].idade}</td>
+            <td>
+                <img class="icone" src="edit.svg" onclick="editar(${cont})">
+                <img class="icone" src="delete.svg" onclick="remover(${cont})">
+            </td>
+        </tr>
+        `
     }
 }
 
-function adicionarConvidado() {
-    let nome = document.getElementById("nome").value
-    let email = document.getElementById("email").value
-    let idade = document.getElementById("idade").value
-    
-    convidados.push({
-        nome: nome,
-        email: email,
-        idade: idade
-    })
+function salvar() {
+    let convidado = {} // Objeto novo
+    convidado.nome = document.getElementById("nome").value
+    convidado.email = document.getElementById("email").value
+    convidado.idade = document.getElementById("idade").value
+
+    // edita ou adiciona
+    if (posicaoEdicao >= 0) {
+        convidados[posicaoEdicao] = convidado
+        posicaoEdicao = undefined
+    } else {
+        convidados.push(convidado)
+    }
+
+    carregarLista()
+    limparFormulario()
+}
+
+function limparFormulario() {
+    document.getElementById("nome").value = ""
+    document.getElementById("email").value = ""
+    document.getElementById("idade").value = ""
+}
+
+function editar(posicao) {
+    document.getElementById("nome").value = convidados[posicao].nome
+    document.getElementById("email").value = convidados[posicao].email
+    document.getElementById("idade").value = convidados[posicao].idade
+    posicaoEdicao = posicao
+}
+
+function cancelar() {
+    limparFormulario()
+    posicaoEdicao = undefined
+}
+
+function remover(posicao) {
+    convidados.splice(posicao, 1)
     carregarLista()
 }
